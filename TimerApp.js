957a715f5cc1d4306c093e14fcc217b1cdba79f8
DontKnowWhack <67173767+DontKnowWhack@users.noreply.github.com>
2025-07-11 00:34:35 -0400

    Added framework and some timer functionality. Much more work is needed to be done before midnight tomorrow.

--- TimerApp.js
+++ TimerApp.js
@@ -1,32 +1,11 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const express = require("express")
+const app = express()
+const port = 3000
 
-const hostname = '0.0.0.0';
-const port = 3000;
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/pomodoro.html");
+})
 
-const server = http.createServer((req, res) => {
-
-    const filePath = path.join(__dirname, 'public', 'index.html');
-    console.log("Attempting to read:", filePath);
-
-  if (req.url === '/' || req.url === '/index.html') {
-    const filePath = path.join(__dirname, 'public', 'index.html');
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Error loading page');
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      }
-    });
-  } else {
-    res.writeHead(404);
-    res.end('Not Found');
-  }
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+app.listen(port, () =>{
+    console.log('App listening on port ${port}')
+})
